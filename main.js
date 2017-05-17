d963d6a9e4433ca4b93a8a8aec1eabc1bcbf13ca
a0066958 <liuyuezhong@u.nus.edu>
2017-05-17 19:57:04 +0800

    update

--- main.js
+++ main.js
@@ -1,56 +1,81 @@
- // init view point for vizicities
-            var coords =[1.306487, 103.835907];
+// Singapore
+var coords = [1.306487, 103.835907];
 
-            var world = VIZI.world('world', {
-                skybox: true,
-                postProcessing: false
-            }).setView(coords);
-            // Set position of sun in sky
-            world._environment._skybox.setInclination(0.3);
+var world = VIZI.world('world', {
+    skybox: true,
+    postProcessing: true
+}).setView(coords);
 
-            // Add controls
-            VIZI.Controls.orbit().addTo(world);
-            // Add fundamental map
-            VIZI.imageTileLayer('http://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png', {
-                attribution: '&copy; <a href="http://www.openstreetmap.org/copyright" >OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
-            }).addTo(world);
+// Set position of sun in sky
+world._environment._skybox.setInclination(0.3);
 
-//for whole singapore roads
-                VIZI.geoJSONLayer('Data/roads.json', {
-                        output: true,
-                        interactive: false,
-                        style: function (feature) {
+// Add controls
+VIZI.Controls.orbit().addTo(world);
 
-                            return {
-                                lineColor: '#f7c616',
-                                lineWidth: 1,
-                                lineTransparent: true,
-                                lineOpacity: 0.2,
-                                lineBlending: THREE.AdditiveBlending,
-                                lineRenderOrder: 2
-                            };
+// Leave a single CPU for the main browser thread
+world.createWorkers(7).then(function() {
+    console.log('Workers ready');
 
-                        }
+    // CartoDB basemap
+    VIZI.imageTileLayer('http://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
+    }).addTo(world);
 
-                    }).addTo(world);
+    //for whole singapore roads
+    VIZI.geoJSONLayer('Data/roads.json', {
+        output: true,
+        interactive: false,
+        style: function (feature) {
 
-                //for whole singapore building layer
-                 VIZI.geoJSONLayer('Data/buildings.json', {
-                         output: true,
-                         interactive: false,
-                         style: function (feature) {
-                
-                
-                             height = 1000 + Math.random() * 300;
-                
-                             return {
-                                 height: height
-                
-                             };
-                
-                         },
-                         filter: function(feature) {
-                             // Don't show points
-                             return feature.geometry.type !== 'Point';
-                         }
-                     }).addTo(world);
+            return {
+                lineColor: '#f7c616',
+                lineWidth: 1,
+                lineTransparent: true,
+                lineOpacity: 0.2,
+                lineBlending: THREE.AdditiveBlending,
+                lineRenderOrder: 2
+            };
+
+        }
+
+    }).addTo(world);
+
+    //for whole singapore building layer
+    VIZI.geoJSONLayer('Data/buildings.json', {
+        output: true,
+        interactive: false,
+        style: function (feature) {
+            var color;
+
+            if (feature.properties.energysum) {
+                height = feature.properties.energysum /10;
+                if(height > 1000) {
+                    color = '#F70C05'
+                }else if (height > 800 & height < 1000)
+                {color = '#f7ce12'}
+                else if (height > 600 & height < 800)
+                {color = '#dff716'}
+                else if (height > 400 & height < 600)
+                {color = '#b7f712'}
+                else if (height > 200 & height < 400)
+                {color = '#b7f712'}
+                else if (height > 0 & height < 200)
+                {color = '#65f717'}
+            } else {
+                height = 10 + Math.random() * 10;
+            }
+
+
+            return {
+                height: height,
+                color: color
+            };
+
+        },
+        filter: function(feature) {
+            // Don't show points
+            return feature.geometry.type !== 'Point';
+        }
+    }).addTo(world);
+
+});
